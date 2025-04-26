667b07b64f219cd50cc3dda21af12f12b7793327
LanaKoshe <LanaKoshe>
2025-04-26 15:38:01 +0300

    aaa

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('nav');
+  
+    burger.addEventListener('click', () => {
+      nav.classList.toggle('active');
+      burger.classList.toggle('open');
+    });
+  });
+  
